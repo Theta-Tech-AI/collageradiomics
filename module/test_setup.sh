8fca16769b28d62e795d83ca390dd85ddcf7e8f9
Nathan Hillyer <nathanielhillyer@gmail.com>
2020-07-05 18:42:26 -0500

    Add dev prefix.

--- module/test_setup.sh
+++ module/test_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-VERSION=`cat VERSION`.$GITHUB_RUN_NUMBER
+VERSION=`cat VERSION`.dev$GITHUB_RUN_NUMBER
 echo $VERSION > VERSION
+echo $VERSION
 rm -rf dist
 python test_setup.py sdist bdist_wheel
 twine upload dist/*

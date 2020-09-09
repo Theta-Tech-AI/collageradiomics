800c6be91d1270b548f6f86d814a37605a17b4e3
Nathan Hillyer <nathanielhillyer@gmail.com>
2020-09-09 14:18:42 -0400

    Rename dev pypi script.

--- module/upload-to-pypi-dev.sh
+++ module/upload-to-pypi-dev.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 VERSION=`cat VERSION`.dev$GITHUB_RUN_NUMBER
 echo $VERSION > VERSION
-echo $VERSION
 rm -rf dist
-python test_setup.py sdist bdist_wheel
+python setup.py sdist bdist_wheel
 twine upload dist/*

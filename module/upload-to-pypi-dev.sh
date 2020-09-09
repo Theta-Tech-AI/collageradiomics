09137901ea52b399e6ee912850ea038409910a10
Nathan Hillyer <nathanielhillyer@gmail.com>
2020-09-09 14:45:21 -0400

    WIP

---
+++ module/upload-to-pypi-dev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Append github action build number to version in VERSION file.
+VERSION=`cat VERSION`.dev$GITHUB_RUN_NUMBER
+echo $VERSION > VERSION
+
+# Remove any straggling binaries.
+rm -rf core/dist
+rm -rf types/dist
+
+# Put wheels and source to dist directories.
+python core/setup.py sdist bdist_wheel
+python types/setup.py sdist bdist_wheel
+
+# Upload binary and source files to PyPI.
+twine upload core/dist/*
+twine upload types/dist/*
